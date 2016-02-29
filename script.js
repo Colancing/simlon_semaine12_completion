816b88d070402edccc12ea2556e7f7b1dd2157ca
titzelle <pseudoups@hotmail.fr>
2016-02-29 19:23:51 +0100

    Ajout en javascript d'une condition pour ne pas envoyer vers php quand le input est egal à "";

--- script.js
+++ script.js
@@ -1,3 +1,82 @@
 /**
  * Created by Emmanuelle on 26/02/2016.
  */
+var findme = document.getElementById("input");
+var divresults = document.getElementById("results");
+var selection = -1;
+var arraydiv = document.getElementsByClassName("choice");
+
+
+function navigate(e) {
+    console.log(e.keyCode);
+    var length = arraydiv.length;
+    if (e.keyCode == 40 && length > 0) {
+        if (selection > -1) {
+            arraydiv[selection].id = "";
+        }
+        if (selection == length - 1) {
+            selection = -1;
+        }
+        selection++;
+        arraydiv[selection].id = "selected";
+    }
+    else if (e.keyCode == 38 && selection > -1) {
+        arraydiv[selection].id = "";
+        if (selection == 0) {
+            selection = length;
+        }
+        selection--;
+        arraydiv[selection].id = "selected";
+    }
+    else if (e.keyCode == 13 && selection > -1) {
+        arraydiv[selection].id = "";
+        findme.value = arraydiv[selection].innerHTML;
+    }
+    else {
+        selection = -1;
+        if (findme.value != 0) {
+            gotophp();
+        }
+    }
+}
+function gotophp() {
+    divresults.innerHTML = "";
+    var xml = new XMLHttpRequest();
+    xml.addEventListener('readystatechange', function () {
+            if (xml.readyState == 4) {
+                if (xml.status === 200 || xml.status === 0) {
+                    var phpback = xml.response;
+                    if (phpback.length) {
+                        var array_proposals = phpback.split("|");
+                        for (var i = 0; i < array_proposals.length; i++) {
+                            var div = document.createElement("div");
+                            div.innerHTML = array_proposals[i];
+                            div.className = "choice";
+                            div.style.cursor = "pointer";
+                            div.addEventListener("click", choosen, true);
+                            divresults.appendChild(div);
+                        }
+                    }
+                    else {
+                        divresults.innerHTML = "Aucune ville ne correspond à cette recherche";
+                    }
+                } else {
+                    alert('error code :' + xml.status + ':' + xml.statusText)
+                }
+            }
+        }
+    )
+    ;
+    xml.open("POST", 'villes.php', true);
+    xml.setRequestHeader("content-Type", "application/x-www-form-urlencoded; charset=UTF-8");
+    xml.send("findme=" + findme.value);
+}
+
+/*Fin de gotophp*/
+
+function choosen() {
+    findme.value = this.innerHTML;
+    divresults.innerHTML = "";
+}
+
+findme.addEventListener("keyup", navigate, true);
--- villes.php
+++ villes.php
@@ -5,6 +5,23 @@
  * Date: 26/02/2016
  * Time: 18:02
  */
-$file = file_get_contents("towns.txt");
-$villes = unserialize($file);
-var_dump($villes);
+$file    = file_get_contents( "towns.txt" );
+$villes  = unserialize( $file );
+$results = [ ];
+$number  = 0;
+$text    = "Aucune ville en correspond à votre recherche";
+
+if ( isset ( $_POST['findme'] ) && $_POST['findme'] != '' ) {
+	$findme = $_POST['findme'];
+
+	foreach ( $villes as $ville ) {
+		$pos = stripos( $ville, $findme );
+		if ( $pos === 0 ) {
+			array_push( $results, $ville );
+			$number ++;
+		}
+		sort( $results );
+		$text = implode( '|', $results );
+	}
+}
+echo $text;
